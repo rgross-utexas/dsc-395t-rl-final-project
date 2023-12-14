c3481cbca1b92b16d4dee2e5ced76381e359eda2
rgross-utexas <rgross@utexas.edu>
2023-12-13 19:43:40 -0500

    Implement ucb

--- projects/rl-an-intro/ch2/k-armed-testbed.py
+++ projects/rl-an-intro/ch2/k-armed-testbed.py
@@ -1,5 +1,5 @@
 import argparse
-from typing import Dict, Optional
+from typing import Dict
 
 from matplotlib import pyplot as plt
 import numpy as np
@@ -23,7 +23,7 @@ def main(args) -> None:
 
     data = do_run(args)
 
-    render_figure(data, args.get('output_filename'))
+    render_figure(data, args)
 
 
 def do_run(args: Dict) -> Dict:
@@ -39,15 +39,22 @@ def do_run(args: Dict) -> Dict:
     initial_value = args.get('initial_value')
     alpha = args.get('alpha')
     run_type = args.get('run_type')
+    explore_type = args.get('explore_type')
+    ucb_consts = args.get('ucb_consts')
 
-    for epsilon in epsilons:
+    if explore_type == 'ucb':
+        values = ucb_consts
+    else:
+        values = epsilons
+
+    for v in values:
 
         # track all the averages as we go
         o_avg = np.zeros(num_steps, dtype=float)
         r_avg = np.zeros(num_steps, dtype=float)
 
         # save the averages for each epsilon
-        data[epsilon] = {'r': r_avg, 'o': o_avg}
+        data[v] = {'r': r_avg, 'o': o_avg}
 
         for run in range(1, num_runs + 1):
 
@@ -69,14 +76,18 @@ def do_run(args: Dict) -> Dict:
 
             for step in range(num_steps):
 
-                # select the arm, possibly at random based on epsilon
-                e = np.random.uniform(0, 1)
-                if e > epsilon:
-                    q_run_max = q_run.max()
-                    choice = np.flatnonzero(q_run == q_run_max)
-                    action = np.random.choice(choice)
+                if explore_type == 'ucb':
+                    action = np.argmax(q_run + v * np.sqrt(np.log(step + 1)/n_run))
                 else:
-                    action = np.random.randint(0, num_arms)
+                    # select the action, possibly at random based on epsilon
+                    e = np.random.uniform(0, 1)
+                    if e > v:
+                        # noinspection PyArgumentList
+                        q_run_max = q_run.max()
+                        choice = np.flatnonzero(q_run == q_run_max)
+                        action = np.random.choice(choice)
+                    else:
+                        action = np.random.randint(0, num_arms)
 
                 # track which action was taken
                 n_run[action] += 1
@@ -97,7 +108,12 @@ def do_run(args: Dict) -> Dict:
     return data
 
 
-def render_figure(data: Dict, output_filename: str):
+def render_figure(data: Dict, args: Dict):
+
+    if args.get('explore_type') == 'ucb':
+        label = 'UCB constant'
+    else:
+        label = 'epsilon'
 
     # render the figure and write it to file
     fig, axes = plt.subplots(2, 1)
@@ -107,15 +123,15 @@ def render_figure(data: Dict, output_filename: str):
     axes[1].set_ylim([0., 1.])
 
     for epsilon, data in data.items():
-        axes[0].plot(data['r'], label=f'epsilon={epsilon}')
-        axes[1].plot(data['o'], label=f'epsilon={epsilon}')
+        axes[0].plot(data['r'], label=f'{label}={epsilon}')
+        axes[1].plot(data['o'], label=f'{label}={epsilon}')
 
     axes[0].legend(loc='lower right')
     axes[0].title.set_text("Average Reward Value")
     axes[1].legend(loc='lower right')
     axes[1].title.set_text("Optimal Action Ratio")
 
-    plt.savefig(output_filename)
+    plt.savefig(args.get('output_filename'))
 
 
 if __name__ == '__main__':
@@ -125,7 +141,7 @@ if __name__ == '__main__':
     parser.add_argument('--num_arms', type=int, default=10)
     parser.add_argument('--mean', type=float, default=0.0)
     parser.add_argument('--std', type=float, default=1.0)
-    parser.add_argument('--num_steps', type=int, default=2000)
+    parser.add_argument('--num_steps', type=int, default=1000)
     parser.add_argument('--num_runs', type=int, default=2000)
     parser.add_argument('--epsilons', nargs='*',
                         default=[.1, .05, .01, 0.0])
@@ -134,6 +150,10 @@ if __name__ == '__main__':
     parser.add_argument('--run_type',
                         choices=['default', 'stationary'],
                         default='default')
+    parser.add_argument('--explore_type',
+                        choices=['e_greedy', 'ucb'],
+                        default='e_greedy')
+    parser.add_argument('--ucb_consts', nargs='*', default=[1, 2, 5, 10])
     parser.add_argument('--output_filename', type=str,
                         default='k-armed-testbed-stat.png')
 
