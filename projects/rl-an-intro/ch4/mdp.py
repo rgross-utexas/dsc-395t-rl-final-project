bb3011d70e4e51608acaa9c9dbdc14aa4c9b2411
rgross-utexas <rgross@utexas.edu>
2023-12-14 21:59:02 -0500

    Refactor a bit (i.e., renames, etc)

--- projects/rl-an-intro/ch4/mdp.py
+++ projects/rl-an-intro/ch4/mdp.py
@@ -1,39 +1,36 @@
-from typing import Tuple
-
 import numpy as np
 
 from ..utils.env import Env, EnvSpec, EnvWithModel
 
 
-class GridWorldConstants:
-    nA = 4
-    LEFT = 0
-    UP = 1
-    RIGHT = 2
-    DOWN = 3
+class ActionConstants(object):
+    NUM_ACTIONS = 4
+    LEFT_ACTION = 0
+    UP_ACTION = 1
+    RIGHT_ACTION = 2
+    DOWN_ACTION = 3
 
 
 class GridWorldMDP(Env):
 
-    def __init__(self, nS: int):
-        env_spec = EnvSpec(nS, GridWorldConstants.nA, 1.)
+    def __init__(self, num_states: int):
+
+        env_spec = EnvSpec(num_states, ActionConstants.NUM_ACTIONS, 1.)
 
         super().__init__(env_spec)
 
-        self.nS = nS
+        self.num_states = num_states
         self._state = None
         self.final_state = 0
         self.trans_mat = self._build_trans_mat()
         self.r_mat = self._build_r_mat()
 
-    def _build_trans_mat(self) -> np.array:
-        raise NotImplementedError("_build_trans_mat")
-
     def reset(self):
-        self._state = np.random.choice(range(1, self.nS))
+        self._state = np.random.choice(range(1, self.num_states))
         return self._state
 
     def step(self, action):
+
         assert action in list(range(self.spec.nA)), "Invalid Action"
         assert self._state != self.final_state, "Episode has ended!"
 
@@ -43,9 +40,12 @@ class GridWorldMDP(Env):
 
         return self._state, r, self._state == self.final_state
 
+    def _build_trans_mat(self) -> np.array:
+        raise NotImplementedError("_build_trans_mat")
+
     def _build_r_mat(self) -> np.array:
 
-        r_mat = np.zeros((self.nS, GridWorldConstants.nA, self.nS))
+        r_mat = np.zeros((self.num_states, ActionConstants.NUM_ACTIONS, self.num_states))
 
         # initialize all rewards to -1
         r_mat[:, :, :] = -1.
@@ -58,29 +58,39 @@ class GridWorldMDP(Env):
 
 class ThreeStateGridWorldMDP(GridWorldMDP):  # MDP introduced at Fig 5.4 in Sutton Book
 
-    nS = 3
+    num_states = 3
+
+    '''
+    +---+---+
+    | F | 1 |
+    +---+---+
+    | 2 | F |
+    +---+---+
+    '''
 
     def __init__(self):
         super().__init__(self.nS)
 
     def _build_trans_mat(self) -> np.array:
 
-        trans_mat = np.zeros((self.nS, GridWorldConstants.nA, self.nS))
+        trans_mat = np.zeros((self.num_states, ActionConstants.NUM_ACTIONS, self.num_states))
 
         # final state
         trans_mat[self.final_state, :, self.final_state] = 1
 
+        # all the actions are deterministic
+
         s = 1
-        trans_mat[s, GridWorldConstants.LEFT, self.final_state] = 1
-        trans_mat[s, GridWorldConstants.UP, s] = 1
-        trans_mat[s, GridWorldConstants.RIGHT, s] = 1
-        trans_mat[s, GridWorldConstants.DOWN, self.final_state] = 1
+        trans_mat[s, ActionConstants.LEFT_ACTION, self.final_state] = 1
+        trans_mat[s, ActionConstants.UP_ACTION, s] = 1
+        trans_mat[s, ActionConstants.RIGHT_ACTION, s] = 1
+        trans_mat[s, ActionConstants.DOWN_ACTION, self.final_state] = 1
 
         s = 2
-        trans_mat[s, GridWorldConstants.LEFT, s] = 1
-        trans_mat[s, GridWorldConstants.UP, self.final_state] = 1
-        trans_mat[s, GridWorldConstants.RIGHT, self.final_state] = 1
-        trans_mat[s, GridWorldConstants.DOWN, s] = 1
+        trans_mat[s, ActionConstants.LEFT_ACTION, s] = 1
+        trans_mat[s, ActionConstants.UP_ACTION, self.final_state] = 1
+        trans_mat[s, ActionConstants.RIGHT_ACTION, self.final_state] = 1
+        trans_mat[s, ActionConstants.DOWN_ACTION, s] = 1
 
         return trans_mat
 
@@ -97,113 +107,115 @@ class ThreeStateGridWorldMDPWithModel(ThreeStateGridWorldMDP, EnvWithModel):
 
 class FifteenStateGridWorldMDP(GridWorldMDP):  # MDP introduced at Fig 5.4 in Sutton Book
 
-    nS = 15
+    num_states = 15
+
+    '''
+    +---+---+---+---+
+    | F | 1 | 2 | 3 |
+    +---+---+---+---+
+    | 4 | 5 | 6 | 7 |
+    +---+---+---+---+
+    | 8 | 9 | 10| 11|
+    +---+---+---+---+
+    | 12| 13| 14| F |
+    +---+---+---+---+
+    '''
 
     def __init__(self):
-        super().__init__(self.nS)
+        super().__init__(self.num_states)
 
     def _build_trans_mat(self) -> np.array:
 
-        trans_mat = np.zeros((self.nS, GridWorldConstants.nA, self.nS))
+        trans_mat = np.zeros((self.num_states, ActionConstants.NUM_ACTIONS, self.num_states))
 
         # final state
         trans_mat[self.final_state, :, self.final_state] = 1
 
-        '''
-        +---+---+---+---+
-        | F | 1 | 2 | 3 |
-        +---+---+---+---+
-        | 4 | 5 | 6 | 7 |
-        +---+---+---+---+
-        | 8 | 9 | 10| 11|
-        +---+---+---+---+
-        | 12| 13| 14| F |
-        +---+---+---+---+
-        '''
+        # all the actions are deterministic
 
         s = 1
-        trans_mat[s, GridWorldConstants.LEFT, self.final_state] = 1
-        trans_mat[s, GridWorldConstants.UP, s] = 1
-        trans_mat[s, GridWorldConstants.RIGHT, s + 1] = 1
-        trans_mat[s, GridWorldConstants.DOWN, s + 4] = 1
+        trans_mat[s, ActionConstants.LEFT_ACTION, self.final_state] = 1
+        trans_mat[s, ActionConstants.UP_ACTION, s] = 1
+        trans_mat[s, ActionConstants.RIGHT_ACTION, s + 1] = 1
+        trans_mat[s, ActionConstants.DOWN_ACTION, s + 4] = 1
 
         s = 2
-        trans_mat[s, GridWorldConstants.LEFT, s - 1] = 1
-        trans_mat[s, GridWorldConstants.UP, s] = 1
-        trans_mat[s, GridWorldConstants.RIGHT, s + 1] = 1
-        trans_mat[s, GridWorldConstants.DOWN, s + 4] = 1
+        trans_mat[s, ActionConstants.LEFT_ACTION, s - 1] = 1
+        trans_mat[s, ActionConstants.UP_ACTION, s] = 1
+        trans_mat[s, ActionConstants.RIGHT_ACTION, s + 1] = 1
+        trans_mat[s, ActionConstants.DOWN_ACTION, s + 4] = 1
 
         s = 3
-        trans_mat[s, GridWorldConstants.LEFT, s - 1] = 1
-        trans_mat[s, GridWorldConstants.UP, s] = 1
-        trans_mat[s, GridWorldConstants.RIGHT, s] = 1
-        trans_mat[s, GridWorldConstants.DOWN, s + 4] = 1
+        trans_mat[s, ActionConstants.LEFT_ACTION, s - 1] = 1
+        trans_mat[s, ActionConstants.UP_ACTION, s] = 1
+        trans_mat[s, ActionConstants.RIGHT_ACTION, s] = 1
+        trans_mat[s, ActionConstants.DOWN_ACTION, s + 4] = 1
 
         s = 4
-        trans_mat[s, GridWorldConstants.LEFT, s] = 1
-        trans_mat[s, GridWorldConstants.UP, self.final_state] = 1
-        trans_mat[s, GridWorldConstants.RIGHT, s + 1] = 1
-        trans_mat[s, GridWorldConstants.DOWN, s + 4] = 1
+        trans_mat[s, ActionConstants.LEFT_ACTION, s] = 1
+        trans_mat[s, ActionConstants.UP_ACTION, self.final_state] = 1
+        trans_mat[s, ActionConstants.RIGHT_ACTION, s + 1] = 1
+        trans_mat[s, ActionConstants.DOWN_ACTION, s + 4] = 1
 
         s = 5
-        trans_mat[s, GridWorldConstants.LEFT, s - 1] = 1
-        trans_mat[s, GridWorldConstants.UP, s - 4] = 1
-        trans_mat[s, GridWorldConstants.RIGHT, s + 1] = 1
-        trans_mat[s, GridWorldConstants.DOWN, s + 4] = 1
+        trans_mat[s, ActionConstants.LEFT_ACTION, s - 1] = 1
+        trans_mat[s, ActionConstants.UP_ACTION, s - 4] = 1
+        trans_mat[s, ActionConstants.RIGHT_ACTION, s + 1] = 1
+        trans_mat[s, ActionConstants.DOWN_ACTION, s + 4] = 1
 
         s = 6
-        trans_mat[s, GridWorldConstants.LEFT, s - 1] = 1
-        trans_mat[s, GridWorldConstants.UP, s - 4] = 1
-        trans_mat[s, GridWorldConstants.RIGHT, s + 1] = 1
-        trans_mat[s, GridWorldConstants.DOWN, s + 4] = 1
+        trans_mat[s, ActionConstants.LEFT_ACTION, s - 1] = 1
+        trans_mat[s, ActionConstants.UP_ACTION, s - 4] = 1
+        trans_mat[s, ActionConstants.RIGHT_ACTION, s + 1] = 1
+        trans_mat[s, ActionConstants.DOWN_ACTION, s + 4] = 1
 
         s = 7
-        trans_mat[s, GridWorldConstants.LEFT, s - 1] = 1
-        trans_mat[s, GridWorldConstants.UP, s - 4] = 1
-        trans_mat[s, GridWorldConstants.RIGHT, s] = 1
-        trans_mat[s, GridWorldConstants.DOWN, s + 4] = 1
+        trans_mat[s, ActionConstants.LEFT_ACTION, s - 1] = 1
+        trans_mat[s, ActionConstants.UP_ACTION, s - 4] = 1
+        trans_mat[s, ActionConstants.RIGHT_ACTION, s] = 1
+        trans_mat[s, ActionConstants.DOWN_ACTION, s + 4] = 1
 
         s = 8
-        trans_mat[s, GridWorldConstants.LEFT, s] = 1
-        trans_mat[s, GridWorldConstants.UP, s - 4] = 1
-        trans_mat[s, GridWorldConstants.RIGHT, s + 1] = 1
-        trans_mat[s, GridWorldConstants.DOWN, s + 4] = 1
+        trans_mat[s, ActionConstants.LEFT_ACTION, s] = 1
+        trans_mat[s, ActionConstants.UP_ACTION, s - 4] = 1
+        trans_mat[s, ActionConstants.RIGHT_ACTION, s + 1] = 1
+        trans_mat[s, ActionConstants.DOWN_ACTION, s + 4] = 1
 
         s = 9
-        trans_mat[s, GridWorldConstants.LEFT, s - 1] = 1
-        trans_mat[s, GridWorldConstants.UP, s - 4] = 1
-        trans_mat[s, GridWorldConstants.RIGHT, s + 1] = 1
-        trans_mat[s, GridWorldConstants.DOWN, s + 4] = 1
+        trans_mat[s, ActionConstants.LEFT_ACTION, s - 1] = 1
+        trans_mat[s, ActionConstants.UP_ACTION, s - 4] = 1
+        trans_mat[s, ActionConstants.RIGHT_ACTION, s + 1] = 1
+        trans_mat[s, ActionConstants.DOWN_ACTION, s + 4] = 1
 
         s = 10
-        trans_mat[s, GridWorldConstants.LEFT, s - 1] = 1
-        trans_mat[s, GridWorldConstants.UP, s - 4] = 1
-        trans_mat[s, GridWorldConstants.RIGHT, s + 1] = 1
-        trans_mat[s, GridWorldConstants.DOWN, s + 4] = 1
+        trans_mat[s, ActionConstants.LEFT_ACTION, s - 1] = 1
+        trans_mat[s, ActionConstants.UP_ACTION, s - 4] = 1
+        trans_mat[s, ActionConstants.RIGHT_ACTION, s + 1] = 1
+        trans_mat[s, ActionConstants.DOWN_ACTION, s + 4] = 1
 
         s = 11
-        trans_mat[s, GridWorldConstants.LEFT, s - 1] = 1
-        trans_mat[s, GridWorldConstants.UP, s - 4] = 1
-        trans_mat[s, GridWorldConstants.RIGHT, s] = 1
-        trans_mat[s, GridWorldConstants.DOWN, self.final_state] = 1
+        trans_mat[s, ActionConstants.LEFT_ACTION, s - 1] = 1
+        trans_mat[s, ActionConstants.UP_ACTION, s - 4] = 1
+        trans_mat[s, ActionConstants.RIGHT_ACTION, s] = 1
+        trans_mat[s, ActionConstants.DOWN_ACTION, self.final_state] = 1
 
         s = 12
-        trans_mat[s, GridWorldConstants.LEFT, s] = 1
-        trans_mat[s, GridWorldConstants.UP, s - 4] = 1
-        trans_mat[s, GridWorldConstants.RIGHT, s + 1] = 1
-        trans_mat[s, GridWorldConstants.DOWN, s] = 1
+        trans_mat[s, ActionConstants.LEFT_ACTION, s] = 1
+        trans_mat[s, ActionConstants.UP_ACTION, s - 4] = 1
+        trans_mat[s, ActionConstants.RIGHT_ACTION, s + 1] = 1
+        trans_mat[s, ActionConstants.DOWN_ACTION, s] = 1
 
         s = 13
-        trans_mat[s, GridWorldConstants.LEFT, s - 1] = 1
-        trans_mat[s, GridWorldConstants.UP, s - 4] = 1
-        trans_mat[s, GridWorldConstants.RIGHT, s + 1] = 1
-        trans_mat[s, GridWorldConstants.DOWN, s] = 1
+        trans_mat[s, ActionConstants.LEFT_ACTION, s - 1] = 1
+        trans_mat[s, ActionConstants.UP_ACTION, s - 4] = 1
+        trans_mat[s, ActionConstants.RIGHT_ACTION, s + 1] = 1
+        trans_mat[s, ActionConstants.DOWN_ACTION, s] = 1
 
         s = 14
-        trans_mat[s, GridWorldConstants.LEFT, s - 1] = 1
-        trans_mat[s, GridWorldConstants.UP, s - 4] = 1
-        trans_mat[s, GridWorldConstants.RIGHT, self.final_state] = 1
-        trans_mat[s, GridWorldConstants.DOWN, s] = 1
+        trans_mat[s, ActionConstants.LEFT_ACTION, s - 1] = 1
+        trans_mat[s, ActionConstants.UP_ACTION, s - 4] = 1
+        trans_mat[s, ActionConstants.RIGHT_ACTION, self.final_state] = 1
+        trans_mat[s, ActionConstants.DOWN_ACTION, s] = 1
 
         return trans_mat
 
