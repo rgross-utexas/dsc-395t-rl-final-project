cf3a24bf0d6fde6e1b033f22448743d98f0acd0b
rgross-utexas <rgross@utexas.edu>
2023-12-24 10:21:01 -0500

    Reformat doc, fix bug in n-step td, and add documentation

--- projects/rl-an-intro/n_step_bootstrapping.py
+++ projects/rl-an-intro/n_step_bootstrapping.py
@@ -1,6 +1,6 @@
 import random
 from collections import defaultdict
-from typing import Iterable, Optional, Tuple
+from typing import List, Optional, Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -17,7 +17,7 @@ INFINITY = 10e10
 
 def on_policy_n_step_td(
         env_spec: EnvSpec,
-        trajs: Iterable[Iterable[Tuple[int, int, int, int]]],
+        trajs: List[List[Tuple[int, int, int, int]]],
         n: int,
         alpha: float,
         init_v: np.array
@@ -39,40 +39,47 @@ def on_policy_n_step_td(
 
     for traj in trajs:
 
-        t_terminal = len(traj)
-        _, _, _, terminal_state = traj[-1]
-        for t, (s_t, a_t, r_t1, s_t1) in enumerate(traj):
+        # get the max t so that we can modulo by it for large number of steps
+        t_max = len(traj)
+        t_terminal = np.inf
 
-            if t < t_terminal and s_t1 == terminal_state:
-                t_terminal = t + 1
+        # get the terminal state from the trajectory
+        _, _, _, terminal_state = traj[-1]
+        t = 0
+        while True:
+
+            # get the data from the trajectory for time t
+            s_t, a_t, r_t1, s_t1 = traj[t % t_max]
+            if t < t_terminal:
+                if s_t1 == terminal_state:
+                    t_terminal = t + 1
 
             tau = t - n + 1
             if tau >= 0:
 
-                g_upper = min(tau + n, t_terminal)
-                g_lower = tau + 1
-
                 g = 0.
-                for i in range(g_lower, g_upper):
-                    _, _, r_i, _ = traj[i]
-                    g += (gamma ** (i - g_lower)) * r_i
+                for i in range(tau + 1, min(tau + n, t_terminal) + 1):
+                    _, _, r_i, _ = traj[i % t_max]
+                    g += (gamma ** (i - (tau + 1))) * r_i
 
                 if tau + n < t_terminal:
-                    s_tau_n, _, _, _ = traj[tau + n]
+                    s_tau_n, _, _, _ = traj[(tau + n) % t_max]
                     g += (gamma ** n) * v[s_tau_n]
 
-                s_tau, _, _, _ = traj[tau]
+                s_tau, _, _, _ = traj[tau % t_max]
                 v[s_tau] += alpha * (g - v[s_tau])
 
             if tau == t_terminal - 1:
                 break
+
+            t += 1
 
     return v
 
 
 if __name__ == '__main__':
 
-    n_trajectories = 100000
+    n_trajectories = 10000
 
     # create a model-free mdp to perform value prediction
     env = GeneralDeterministicGridWorldMDP(4, 4)
@@ -81,6 +88,7 @@ if __name__ == '__main__':
     # generate trajectories from behavior policy
     trajs = generate_trajectories(env, behavior_policy, n_trajectories)
 
-    for n_steps in [1, 2]:
+    n_steps_list = [1, 2, 5, 10, 20, 50, 100]
+    for n_steps in n_steps_list:
         v = on_policy_n_step_td(env.spec, trajs, n_steps, .005, np.zeros(env.spec.num_states))
-        print(f'{v=}')
+        print(f'Number of steps: {n_steps}, number of episodes/trajectories: {n_trajectories}, {v}')
