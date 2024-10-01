8c92a4c3b489b296562e542dd5282c54d28fd0f6
rgross-utexas <rgross@utexas.edu>
2024-10-01 11:22:22 -0400

    Remove extra line and add TODO

--- projects/rl-an-intro/logger.py
+++ projects/rl-an-intro/logger.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import logging
 
+# TODO: this is reinforce specific, so make it more generic
 # set up simple logging
 logging.basicConfig(filename=f'log/reinforce_{datetime.now().strftime("%Y%m%d_%H%M%S")}.log',
                     format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
